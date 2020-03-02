52686619a1ac0c84b795366136ae4e677e9abdfa
Jesus Triano <core.jesust@gmail.com>
2020-03-02 17:23:49 +0100

    Agregado ejercicio del tema 5 de objetos

--- README.md
+++ README.md
@@ -1,3 +1,38 @@
 # Proyecto App Banco
 
 En este repositorio iremos construyendo el front-end de la aplicación banco, cuyo back-end está realizado en Java.
+
+## Entidades
+
+### Gestor
+
+* id : int(11)
+* usuario : char(20)
+* password : char(64)
+* correo : char(50)
+
+### Cliente
+
+* id : int(11)
+* id_gestor : int(11) --> id tabla gestor
+* usuario : char(20)
+* password : char(64)
+* correo : char(50)
+* saldo : double
+
+### Mensaje
+
+* id : int(11)
+* id_origen : int(11) --> id tabla gestor
+* id_destino : int(11) --> id tabla gestor
+* texto : char(140)
+* fecha : datetime
+
+### Transferencia
+
+* id : int(11)
+* id_ordenante : int(11) --> id tabla cliente
+* id_beneficiario : int(11) --> id tabla cliente
+* importe : double
+* concepto : char(40)
+* fecha : datetime
---
+++ main.js
@@ -0,0 +1,45 @@
+// objetos gestores
+const gestor1 = {
+  id: 1,
+  usuario: 'gestor1',
+  password: 'gestor1',
+  correo: 'gestor1@mail.com'
+};
+const gestor2 = {
+  id: 2,
+  usuario: 'gestor2',
+  password: 'gestor2',
+  correo: 'gestor2@mail.com'
+};
+const gestor3 = {
+  id: 3,
+  usuario: 'gestor3',
+  password: 'gestor3',
+  correo: 'gestor3@mail.com'
+};
+
+// objetos clientes
+const cliente1 = {
+  id: 1,
+  id_gestor: 2,
+  usuario: 'cliente1',
+  password: 'cliente1',
+  correo: 'cliente1@gmail.com'
+  saldo: 3000
+};
+const cliente2 = {
+  id: 2,
+  id_gestor: 1,
+  usuario: 'cliente2',
+  password: 'cliente2',
+  correo: 'cliente2@gmail.com'
+  saldo: 5000
+};
+const cliente3 = {
+  id: 3,
+  id_gestor: 2,
+  usuario: 'client31',
+  password: 'cliente3',
+  correo: 'cliente3@gmail.com'
+  saldo: 1000
+};
