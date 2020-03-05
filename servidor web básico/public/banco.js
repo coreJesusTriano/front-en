dfae72823ca0783f39dd8281a3a321d324aac559
Jesus Triano <core.jesust@gmail.com>
2020-03-05 19:22:10 +0100

    Agregada invocación 'main.js' e implementación 'banco.js' de la funcion loginGestor

--- servidor web básico/public/banco.js
+++ servidor web básico/public/banco.js
@@ -1,7 +1,9 @@
 // Definicion constantes
 const SERVER ='http://localhost:8085';
 const URL_OK = `${SERVER}/ok`;
+const URL_LOGIN_GESTOR = `${SERVER}/login/gestor/`;
 
+// declaración de la función ok
 // function ok(callback(err,datos)) 
 const ok = (callback) => {
 
@@ -11,7 +13,32 @@ const ok = (callback) => {
   }
 
   http(opciones, (err, response) => {
-    if (err) callback(err); // retorna el error
-    callback(null, response); // retorna la respuesta
+    if (err) return callback(err); // retorna el error
+    callback(null, response); // retorna la respuesta "return" no necesario al ser última instrucción
   });  
 };
+
+// declaración de la función loginGestor
+// function loginGestor(usuario, password, callback(err))
+function loginGestor(usuario, password, callback) {
+
+  const opcionesLogin = {
+    url: URL_LOGIN_GESTOR,
+    metodoHTTP: 'POST',
+    cabeceras: {
+      "Content-Type": 'application/x-www-form-urlencoded'
+    },
+    json: true,
+    body: `usuario=${usuario}&password=${password}`
+  };
+  // http(opciones, callback(err, response));
+  http(opcionesLogin, (err, response)=>{
+    if(err) return callback(err);
+    if(!response.ok) return callback(response.msg);
+    // si todo fue correcto
+    // guardo token en el localStorage del navegador, que es una db key:value
+    localStorage.setItem('tokenGestor', response.data.token);
+
+    callback(null); // o callback();
+  });
+} // fin loginGestor
--- servidor web básico/public/main.js
+++ servidor web básico/public/main.js
@@ -51,9 +51,6 @@ function EjercicioLogin() {
   });
 
 }
-// COMIENZO EJECUCIÓN SCRIPT
-//ExeEjercicioOk();
-//EjercicioLogin();
 
 /*
   La estructura habitual de un proyecto será la siguiente:
@@ -63,8 +60,25 @@ function EjercicioLogin() {
   - main.js == script general que ejecutará nuestra aplicación.
  */
 
+// COMIENZO EJECUCIÓN SCRIPT
+
+
+// Invocación a la función ok
 // ok(callback);
 ok((err, datos)=>{
   if (err) return console.log(err);
   console.log(datos);
+});
+
+// Invocación a la función loginGestor
+// loginGestor(usuario, password, callback);
+const usuario = 'gestor1';
+const password = 'gestor1';
+loginGestor(usuario, password, (err)=>{
+  if(err) {
+    // no estoy autenticado
+    return console.log(err);
+  }
+  // estoy autenticado
+  console.log('Estoy autenticado');
 });
